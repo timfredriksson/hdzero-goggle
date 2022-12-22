a762e178d24a24588df88b064521015996ec1929
bkleiner <b.kleiner@str8labs.com>
2022-12-22 15:56:05 +0100

    switch to buildroot toolchain

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ set -e
 if [[ ! -d toolchain ]]; then
 	echo "downloading toolchain..."
 	mkdir toolchain
-	wget -qO- http://musl.cc/arm-linux-musleabihf-cross.tgz | tar xz -C toolchain
+	wget -O- https://toolchains.bootlin.com/downloads/releases/toolchains/armv7-eabihf/tarballs/armv7-eabihf--musl--stable-2021.11-1.tar.bz2 | tar xj --strip-components=1 -C toolchain
 fi
 
 rm -rf build && mkdir build
-cmake . -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -Bbuild
+cmake . -DCMAKE_TOOLCHAIN_FILE=toolchain/share/buildroot/toolchainfile.cmake -Bbuild
